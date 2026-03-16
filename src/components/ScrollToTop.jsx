b51a1d41063c24993f00f452cb1a912b08b78804
Evik34 <kacevik34@gmail.com>
2026-03-16 17:19:42 +0500

    Update ScrollToTop.jsx

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -5,6 +5,7 @@ export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
+    console.log("ScrollToTop сработал, путь:", pathname);
     window.scrollTo(0, 0);
   }, [pathname]);
 
