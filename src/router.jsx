96ec6132a233ffcae1ecd6beeb6b196deb984e80
Evik34 <kacevik34@gmail.com>
2026-03-16 16:52:22 +0500

    2
    2

--- src/router.jsx
+++ src/router.jsx
@@ -1,20 +1,19 @@
 import { createBrowserRouter } from "react-router-dom"
-
 import Home from "./pages/Home"
 import ProjectAI from "./pages/ProjectAI"
 import ProjectCameras from "./pages/ProjectCameras"
 import ProjectHogwarts from "./pages/ProjectHogwarts"
 
-const router = createBrowserRouter([
-
-{ path: "/", element: <Home/> },
-
-{ path: "/ai-psychologist", element: <ProjectAI/> },
-
-{ path: "/camera-catalog", element: <ProjectCameras/> },
-
-{ path: "/hogwarts-system", element: <ProjectHogwarts/> }
-
-])
+const router = createBrowserRouter(
+  [
+    { path: "/", element: <Home/> },
+    { path: "/ai-psychologist", element: <ProjectAI/> },
+    { path: "/camera-catalog", element: <ProjectCameras/> },
+    { path: "/hogwarts-system", element: <ProjectHogwarts/> }
+  ],
+  {
+    basename: '/evelinakoneva.github.io' 
+  }
+)
 
 export default router
