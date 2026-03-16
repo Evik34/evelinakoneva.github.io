c7c9ac16110e1bbab500bc5652ebaadbd5ea00fc
Evik34 <kacevik34@gmail.com>
2026-03-16 17:09:28 +0500

    3 commit
    исправлен проскролл вниз при попадании на страницу

--- src/router.jsx
+++ src/router.jsx
@@ -1,19 +1,26 @@
-import { createBrowserRouter } from "react-router-dom"
-import Home from "./pages/Home"
-import ProjectAI from "./pages/ProjectAI"
-import ProjectCameras from "./pages/ProjectCameras"
-import ProjectHogwarts from "./pages/ProjectHogwarts"
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import ProjectAI from "./pages/ProjectAI";
+import ProjectCameras from "./pages/ProjectCameras";
+import ProjectHogwarts from "./pages/ProjectHogwarts";
 
 const router = createBrowserRouter(
   [
-    { path: "/", element: <Home/> },
-    { path: "/ai-psychologist", element: <ProjectAI/> },
-    { path: "/camera-catalog", element: <ProjectCameras/> },
-    { path: "/hogwarts-system", element: <ProjectHogwarts/> }
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "ai-psychologist", element: <ProjectAI /> },
+        { path: "camera-catalog", element: <ProjectCameras /> },
+        { path: "hogwarts-system", element: <ProjectHogwarts /> },
+      ],
+    },
   ],
   {
-    basename: '/evelinakoneva.github.io' 
+    basename: '/evelinakoneva.github.io'
   }
-)
+);
 
-export default router
+export default router;
